96b7c8e0f2033c381a50741dd64de63f45c2d9f8
horwengliang <horwengliang@gmail.com>
2019-12-05 20:31:19 +0800

    v1.0.0-alpha.8

--- server/graphql/resolvers/transport-vehicle/transport-vehicles.ts
+++ server/graphql/resolvers/transport-vehicle/transport-vehicles.ts
@@ -1,13 +1,10 @@
-import { getPermittedBizplaceIds } from '@things-factory/biz-base'
 import { convertListParams, ListParam } from '@things-factory/shell'
-import { getRepository, In } from 'typeorm'
+import { getRepository } from 'typeorm'
 import { TransportVehicle } from '../../../entities'
 
 export const transportVehiclesResolver = {
   async transportVehicles(_: any, params: ListParam, context: any) {
     const convertedParams = convertListParams(params)
-    convertedParams.where.bizplace = In(await getPermittedBizplaceIds(context.state.domain, context.state.user))
-
     const [items, total] = await getRepository(TransportVehicle).findAndCount({
       ...convertedParams,
       relations: ['domain', 'creator', 'updater']
